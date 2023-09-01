d7068cc3259f8344b88f39abf8f131fdbdb8f5c9
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-01 15:46:56 +0500

    Update user_authentication.py

--- modules/user_authentication.py
+++ modules/user_authentication.py
@@ -1,1 +1,23 @@
-# User authentication code 
+# user_authentication.py
+from user_identification import classify_user
+from message_verification import verify_message_content, verify_sender
+
+def authenticate_user(phone_number, message_content):
+    if not phone_number:
+        return False, "Phone number is missing"
+    
+    if not message_content:
+        return False, "Message content is missing"
+
+    user_type = classify_user(phone_number)
+    is_verified, error = verify_message_content(message_content)
+    is_sender_verified = verify_sender(phone_number)
+    
+    if not is_sender_verified:
+        return False, "Invalid phone number format"
+    
+    if not is_verified:
+        return False, error
+
+    return True, f"User is a {user_type} and is authenticated."
+
