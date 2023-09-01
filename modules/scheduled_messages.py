8e7ceddc5aafc113350a496b600d3351f4e4011f
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-01 15:44:12 +0500

    Update scheduled_messages.py

--- modules/scheduled_messages.py
+++ modules/scheduled_messages.py
@@ -1,1 +1,33 @@
-# Scheduled messages code 
+# scheduled_messages.py
+import sched
+import time
+
+# Placeholder for sending a message function
+# In a real-world scenario, this would be integrated with the WhatsApp API to send a message.
+def send_message_to_group(message):
+    print(f"Sending message: {message}")
+
+# Scheduler
+s = sched.scheduler(time.time, time.sleep)
+
+def schedule_message(message, delay_seconds):
+    """
+    Schedule a message to be sent out after a specific delay.
+
+    Args:
+    - message: The content of the message to be sent.
+    - delay_seconds: The delay after which the message should be sent, in seconds.
+    """
+    s.enter(delay_seconds, 1, send_message_to_group, (message,))
+
+def run_schedule():
+    """
+    Run the scheduler to send out all scheduled messages.
+    """
+    s.run()
+
+if __name__ == "__main__":
+    # Placeholder: Testing the scheduled message function.
+    schedule_message("This is a scheduled message to the group.", 5)  # Send after 5 seconds
+    run_schedule()
+
