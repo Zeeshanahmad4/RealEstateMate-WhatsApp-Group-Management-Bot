3dd1c10661226dba36c9c6b630b49aac688dc729
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-01 15:47:37 +0500

    Update user_identification.py

--- modules/user_identification.py
+++ modules/user_identification.py
@@ -1,1 +1,16 @@
-# User identification code 
+# user_identification.py
+
+def classify_user(phone_number):
+    if not phone_number:
+        return "Unknown"
+
+    # Using the last digit of the phone number to classify the user
+    last_digit = phone_number[-1]
+
+    if last_digit == '0' or last_digit == '2':
+        return 'Realtor'
+    elif last_digit == '1' or last_digit == '3':
+        return 'Seller or Lessor'
+    else:
+        return 'Buyer or Tenant'
+
